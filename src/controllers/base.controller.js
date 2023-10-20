0e7442d00ac1674e573172439193fcb5aa8b6b76
Furkan Çakıcı <furkancakici25@gmail.com>
2023-10-20 13:36:10 +0300

    chore: controller update

--- src/controllers/base.controller.js
+++ src/controllers/base.controller.js
@@ -11,40 +11,40 @@ class BaseController {
         res.status(HttpStatusCodes.OK).json(entities)
     })
 
-    getById = async (req, res) => {
+    getById = errorHandler(async (req, res) => {
         const entity = await this.service.getById(req.params.id)
         res.status(HttpStatusCodes.OK).json(entity)
-    }
+    })
 
-    create = async (req, res) => {
+    create = errorHandler(async (req, res) => {
         const newEntity = await this.service.create(req.body)
         res.status(HttpStatusCodes.CREATED).json(newEntity)
-    }
+    })
 
-    delete = async (req, res) => {
+    delete = errorHandler(async (req, res) => {
         const entity = await this.service.deleteById(req.params.id)
         res.status(HttpStatusCodes.OK).json(entity)
-    }
+    })
 
-    update = async (req, res) => {
+    update = errorHandler(async (req, res) => {
         const entity = await this.service.update(req.params.id, req.body)
         res.status(HttpStatusCodes.OK).json(entity)
-    }
+    })
 
-    search = async (req, res) => {
+    search = errorHandler(async (req, res) => {
         const entities = await this.service.search(req.query.q)
         res.status(HttpStatusCodes.OK).json(entities)
-    }
+    })
 
-    count = async (req, res) => {
+    count = errorHandler(async (req, res) => {
         const count = await this.service.count()
         res.status(HttpStatusCodes.OK).json(count)
-    }
+    })
 
-    aggregate = async (req, res) => {
+    aggregate = errorHandler(async (req, res) => {
         const entities = await this.service.aggregate(req.body)
         res.status(HttpStatusCodes.OK).json(entities)
-    }
+    })
 }
 
 export default BaseController
